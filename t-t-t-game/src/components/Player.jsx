292bc2164872aa325927079e68820a0d420de3a6
psdekate <piyushdekate29@gmail.com>
2025-01-06 21:17:20 +0530

    setting up name inputs

---
+++ t-t-t-game/src/components/Player.jsx
@@ -0,0 +1,30 @@
+import { useState } from "react";
+
+export function Player({ initialName, symbol }) {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleNameChange(e) {
+    setPlayerName(e.target.value);
+  }
+
+  let nameInput = <h3 className="player-name">{playerName}</h3>;
+
+  if (isEditing) {
+    nameInput = <input type="text" required value={playerName} onChange={handleNameChange} />;
+  }
+
+  function handleEditing() {
+    setIsEditing((editing) => !editing);
+  }
+
+  return (
+    <div className="player">
+      {nameInput}
+      <h3>{symbol}</h3>
+      <button className="editButton" onClick={handleEditing}>
+        {isEditing ? "Save" : "Edit"}
+      </button>
+    </div>
+  );
+}
