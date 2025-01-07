e224c6fdc8884448dab540132d0f46a98a67b598
psdekate <piyushdekate29@gmail.com>
2025-01-08 01:19:30 +0530

    logical rendering

--- t-t-t-game/src/components/Gameboard.jsx
+++ t-t-t-game/src/components/Gameboard.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialGameBoard = [
   [null, null, null],
   [null, null, null],
@@ -5,14 +7,29 @@ const initialGameBoard = [
 ];
 
 export function Gameboard() {
+  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+  function handleButtonClick(row, col) {
+    setGameBoard((prevGameBoard) => {
+      const updatedBoard = [...prevGameBoard.map((initialArray) => [...initialArray])];
+      updatedBoard[row][col] = "X";
+      return updatedBoard;
+    });
+  }
+
   return (
     <>
       <div className="gameBoard">
-        {initialGameBoard.map((row, rowIndex) => (
+        {gameBoard.map((row, rowIndex) => (
           <div key={rowIndex} className="buttonRow">
             {row.map((playerSymbol, colIndex) => (
               <div key={colIndex}>
-                <button className="clickButton">{playerSymbol}</button>
+                <button
+                  className="clickButton"
+                  onClick={() => handleButtonClick(rowIndex, colIndex)}
+                >
+                  {playerSymbol}
+                </button>
               </div>
             ))}
           </div>
